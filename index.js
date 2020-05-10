9c4f9d979c7709b048d1241152d05a0f0b562b21
Erick Saravia <ericks@labs.aeegle.com>
2020-05-10 09:53:57 -0600

    creando minimizando recursos para el chat

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const http = require("http")
+const socketClusterServer = require('socketcluster-server');
+
+const port = process.env.PORT || "5000";
+
+app.use(express.static(path.join(__dirname, "public")))
+app.get("/", (req, res) => {
+  return res.json("hola")
+})
+
+const httpServer = http.createServer(app)
+let agServer = socketClusterServer.attach(httpServer);
+
+
+
+(async () => {
+  for await (let { socket } of agServer.listener("connection")) {
+    console.log("hay una conexion")
+  }
+})();
+httpServer.listen(port, () => console.log(`Server running on port ${port}`))
